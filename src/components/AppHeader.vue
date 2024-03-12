9c4fdc5bb77b882f5ee8aab22a3e8a482fa6ae5f
OlgaPetrova17 <petrova1783@gmail.com>
2024-03-12 15:06:38 +0100

    Add v-toolbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        Hi
-    </div>
+   <v-app>
+        <v-toolbar app dark class="primary" image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+            <v-toolbar-title v-text="'My VueJs'"></v-toolbar-title>
+        </v-toolbar>
+   </v-app>
 </template>
 
 <script>
