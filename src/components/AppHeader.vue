f23bf5c6612a0784c72a830f8b8b1434653bdf1f
OlgaPetrova17 <petrova1783@gmail.com>
2024-03-14 10:59:14 +0100

    Add button with picture

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-   <v-app>
+    <v-app>
         <v-toolbar app dark class="primary" image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
             <v-toolbar-title v-text="'My VueJs'"></v-toolbar-title>
             <v-spacer></v-spacer>
@@ -9,60 +9,58 @@
                     {{ item.title }}
                     </v-btn> -->
 
-                    <v-btn> <svg-icon type="mdi" :path="path"></svg-icon> Моя кнопка</v-btn>
+                <v-btn flat: key="'menuItem1'" :to="'/about'"> <v-icon icon="$vuetify"></v-icon> Моя кнопка</v-btn>
 
-
-
-                    <!-- <p style="color: bisque;">Текст 1 </p>
+                <!-- <p style="color: bisque;">Текст 1 </p>
                     <p>Текст 2 </p>                    
                     <p>Текст 3 </p>                    
-                    <p v-for="(b) in menuItems" :key="b.icon" style="color: bisque;" >{{ b.title }}</p> -->                    
+                    <p v-for="(b) in menuItems" :key="b.icon" style="color: bisque;" >{{ b.title }}</p> -->
             </v-toolbar-items>
         </v-toolbar>
-   </v-app>
+    </v-app>
 </template>
 
 <script>
-    export default {
-        computed: {
-            menuItems(){
-                return[
-                    {
-                        icon: 'visibility',
-                        title: 'Читать',
-                        route: '/books',
-                    },
-                    {
-                        icon: 'extention',
-                        title: 'Учить слова',
-                        route: '/words',
-                    },
-                    {
-                        icon: 'account_circle',
-                        title: 'Мой кабинет',
-                        route: '/profile',
-                    },
-                    {
-                        icon: 'exit_to_app',
-                        title: 'Выход',
-                        route: '/logout',
-                    },
-                    {
-                        icon: 'input',
-                        title: 'Войти',
-                        route: '/signin',
-                    },
-                    {
-                        icon: 'lock_open',
-                        title: 'Зарегистрироваться',
-                        route: '/signup',
-                    }
-                ]
-            }
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAccount } from '@mdi/js'
+export default {
+    computed: {
+        menuItems() {
+            return [
+                {
+                    icon: 'visibility',
+                    title: 'Читать',
+                    route: '/books',
+                },
+                {
+                    icon: 'extention',
+                    title: 'Учить слова',
+                    route: '/words',
+                },
+                {
+                    icon: 'account_circle',
+                    title: 'Мой кабинет',
+                    route: '/profile',
+                },
+                {
+                    icon: 'exit_to_app',
+                    title: 'Выход',
+                    route: '/logout',
+                },
+                {
+                    icon: 'input',
+                    title: 'Войти',
+                    route: '/signin',
+                },
+                {
+                    icon: 'lock_open',
+                    title: 'Зарегистрироваться',
+                    route: '/signup',
+                }
+            ]
         }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
